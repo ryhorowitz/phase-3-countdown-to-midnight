045fac56b772bd688405a2ba9408453dd12c7832
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-03-16 22:26:34 -0400

    commit tree 90d024817241e84cb567376db7ef5381451bcddb

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown_with_sleep i
+  sleep i
+end
+
+def countdown num
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    countdown_with_sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
